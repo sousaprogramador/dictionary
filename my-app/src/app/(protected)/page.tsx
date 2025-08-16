2b556f5e51f9762b2278317942528b31445de561
Mateus Sousa <sousa.programador@gmail.com>
2025-08-16 12:33:07 -0300

    refactor front

--- my-app/src/app/(protected)/page.tsx
+++ my-app/src/app/(protected)/page.tsx
@@ -1,6 +1,10 @@
 import DictionaryShell from '@/components/DictionaryShell';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  const token = cookies().get('token')?.value;
+  if (!token) redirect('/signin');
   return (
     <main>
       <DictionaryShell />
